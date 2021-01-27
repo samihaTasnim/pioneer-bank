c4415bbf04dae84dde1a0b2cc9a8e6abfde4765e
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-01-27 17:53:54 +0600

    added javascript

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.getElementById("login-btn").addEventListener("click", () => {
     document.getElementById("login-area").style.display = "none";
+    document.getElementById("transaction-area").style.display = "block"
   })  
