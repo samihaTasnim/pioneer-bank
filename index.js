cf9fd3b0e9307440d7dcfd984167a91f6fd5aeac
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-01-28 18:52:00 +0600

    project done

--- index.js
+++ index.js
@@ -2,3 +2,34 @@
    document.getElementById("login-area").style.display = "none";
    document.getElementById("transaction-area").style.display = "block";
 })  
+
+function sumAmount(id, idOfBtn) {
+  let input = parseFloat(document.getElementById(idOfBtn).value);
+  let prevBalance = parseFloat(document.getElementById(id).innerText); 
+  let total = prevBalance + input;
+  if(idOfBtn === "withdrawAmountInput" && id === "balanceAmount") {
+    total = prevBalance - input;
+  }
+  document.getElementById(id).innerText = total;
+}
+
+document.getElementById("addDeposit").addEventListener("click", function() {
+
+  if(document.getElementById("depositNum").value === "") {
+    alert("Please add a number")
+    document.getElementById("depositValue").innerText = inputValue;
+    return null;
+  };
+  
+  sumAmount("depositValue", "depositNum")
+  sumAmount("balanceAmount", "depositNum")
+  document.getElementById("depositNum").value = "";
+})
+
+document.getElementById("withdraw").addEventListener("click", function() {
+  console.log("kala")
+  sumAmount("withdrawAmount", "withdrawAmountInput")
+  sumAmount("balanceAmount", "withdrawAmountInput")
+
+  document.getElementById("withdrawAmountInput").value = "";
+})
